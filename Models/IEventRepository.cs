37c3386a771916ca838e7a6ac131ac1f21289c26
daniel karll <danieljkarll@gmail.com>
2020-02-17 18:50:11 -0600

    Lab 05

---
+++ Models/IEventRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace Modas.Models
+{
+    public interface IEventRepository
+    {
+        IQueryable<Event> Events { get; }
+        IQueryable<Location> Locations { get; }
+        Event AddEvent(Event evt);
+        void DeleteEvent(int eventId);
+
+        Event UpdateEvent(Event evt);
+        void PatchEvent(int id, JsonPatchDocument<Event> patch);
+
+
+    }
+}
